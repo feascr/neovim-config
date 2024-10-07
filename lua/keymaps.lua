ec641ba12037a966aef4312676d860468bd2f022
Artem Petrov <apetr654@gmail.com>
2024-10-08 01:18:31 +0200

    update keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -16,7 +16,7 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>nw", vim.cmd.Ex, { desc = "netrw" })
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
--- add empty line below aboce
+vim.keymap.set("x", "<leader>p", '"_dP', { noremap = true })
